f1007e5554146d544c201d9ca9f7f116b73a1fd2
Juarez Barbosa Junior <jujunio@microsoft.com>
2020-08-23 15:24:16 +0100

    @juarezjuniorgithub

--- Greetings/greetings.go
+++ Greetings/greetings.go
@@ -7,13 +7,20 @@ import (
 
 func main() {
 
-	fmt.Println("Hello World")
+	fmt.Println("Hi there! Type your name and age:")
 
 	var myName string
+	var age int = 45
+	var golangOrg string = "Visit golang.org"
 
 	fmt.Scanln(&myName)
 	myName = strings.TrimSpace(myName)
+	fmt.Scanln(&age)
 
 	fmt.Printf("Hi %s, I'm Go!", myName)
+	fmt.Println("")
+	fmt.Printf("Your age is %d", age)
+	fmt.Println("")
+	fmt.Println(golangOrg)
 
 }
